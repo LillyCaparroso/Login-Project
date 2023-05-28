19a96ec4472ca60f4fad872263dc851a19160c5d
Lilly Caparroso <lilimarrerocaparroso@gmail.com>
2023-05-28 18:47:21 +0100

    Feat(Login): creada página de login con formulario de inicio de sesión

---
+++ src/pages/Login.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { auth, signInWithEmailAndPassword } from "../api/ConfigFirebase";
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const { user } = userCredential;
+        const isAdmin = user.email === "sergio@gmail.com";
+
+        if (isAdmin) {
+          navigate("/administrador");
+        } else {
+          navigate("/usuario");
+        }
+      })
+      .catch((error) => {
+        setErrorMessage("Incorrect e-mail or password");
+      });
+  };
+
+  return (
+    <div className="login-container">
+      <img alt="Logo" className="img-salesianos" />
+      <form className="login-form" onSubmit={handleLogin}>
+        <label className="login-label" htmlFor="email">
+          Email:
+        </label>
+        <input
+          className="login-input"
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <label className="login-label" htmlFor="password">
+          Password:
+        </label>
+        <input
+          className="login-input"
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button className="login-buton" type="submit">
+          Login
+        </button>
+      </form>
+      {errorMessage && <p>{errorMessage}</p>}
+    </div>
+  );
+}
+
+export default Login;
